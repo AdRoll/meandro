a9c2c1128cb3f9ef937f490f5f4480ea7f28d24a
Pablo Costas Sánchez <pablo.costas@nextroll.com>
2022-07-25 11:33:52 +0200

    Lower the minimum Elixir required version to run the project

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Meandro.MixProject do
       app: :meandro,
       description: "The Elixir dead code cleaner",
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       main: "Mix.Tasks.Meandro",
