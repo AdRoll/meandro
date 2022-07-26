10ce5889fb222e417b25e84dea075d7f53e3e3ea
Pablo Costas Sánchez <pablo.costas@nextroll.com>
2022-07-26 13:04:34 +0200

    Run formatter

--- test/rules/unused_callbacks/unused_callbacks_test.exs
+++ test/rules/unused_callbacks/unused_callbacks_test.exs
@@ -7,7 +7,10 @@ defmodule MeandroTest.Rule.UnusedCallbacks do
     file1 = "implementation.exs"
     file2 = "uri_parser_behaviour.exs"
     files_and_asts = [{file1, []}, {file2, []}]
-    [implementation_analyze_result, uri_parser_analyze_result] = UnusedCallback.analyze(files_and_asts, [])
+
+    [implementation_analyze_result, uri_parser_analyze_result] =
+      UnusedCallback.analyze(files_and_asts, [])
+
     assert %{:file => ^file1} = implementation_analyze_result
     assert %{:file => ^file2} = uri_parser_analyze_result
   end
