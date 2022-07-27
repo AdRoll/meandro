aadc7138a0eedf77b5748d37822a42bd6e3bc5b5
Pablo Lopez <pablo.lopez@nextroll.com>
2022-07-27 16:43:40 +0200

    [11-unused] WIP: unused struct fields

--- lib/meandro/rules/unused_struct_field.ex
+++ lib/meandro/rules/unused_struct_field.ex
@@ -20,6 +20,10 @@ defmodule Meandro.Rule.UnusedStructField do
   end
 
   @impl true
+  def is_ignored?(module, module) do
+    true
+  end
+
   def is_ignored?(_pattern, _ignore_spec) do
     false
   end
