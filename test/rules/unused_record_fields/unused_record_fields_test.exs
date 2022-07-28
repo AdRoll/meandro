1353c1fd8edaa28a9133faaaa34e9cff1e03b128
Pablo Brud <pbrudnick@gmail.com>
2022-07-28 13:14:36 -0300

    Tests cleaning (#38)
    * Tests cleaning
    
    * More merge
    
    * More merge
    
    * Unintended to commit this file

--- test/rules/unused_record_fields/unused_record_fields_test.exs
+++ test/rules/unused_record_fields/unused_record_fields_test.exs
@@ -1,4 +1,4 @@
-defmodule MeandroTest.Rule.UnusedRecordFields do
+defmodule MeandroTest.UnusedRecordFields do
   use ExUnit.Case
 
   alias Meandro.Rule
@@ -7,19 +7,19 @@ defmodule MeandroTest.Rule.UnusedRecordFields do
   @test_directory_path "test/rules/unused_record_fields/"
 
   test "emits no warnings on files without records" do
-    files_and_asts = parse_files(["none.exs"])
+    files_and_asts = TestHelpers.parse_files([@test_directory_path <> "none.exs"])
     assert [] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "emits no warnings on files where all field records are used" do
-    files_and_asts = parse_files(["good.exs"])
+    files_and_asts = TestHelpers.parse_files([@test_directory_path <> "good.exs"])
     assert [] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "emits warnings on files where a record has unused field(s)" do
-    file = "bad.exs"
-    module = read_module_name(file)
-    files_and_asts = parse_files([file])
+    file = @test_directory_path <> "bad.exs"
+    module = TestHelpers.read_module_name(file)
+    files_and_asts = TestHelpers.parse_files([file])
     record = read_records(file)
     [{camel_name, atom_name, fields}] = record
     [unused_field1, unused_field2] = fields
@@ -31,29 +31,36 @@ defmodule MeandroTest.Rule.UnusedRecordFields do
       "Record :#{atom_name} (#{camel_name}) has an unused field in the module: #{unused_field2}"
 
     assert [
-             %Meandro.Rule{
-               file: @test_directory_path <> ^file,
+             %Rule{
+               file: ^file,
                line: 3,
                module: ^module,
                pattern: {^atom_name, ^unused_field1},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text1
              },
-             %Meandro.Rule{
-               file: @test_directory_path <> ^file,
+             %Rule{
+               file: ^file,
                line: 3,
                module: ^module,
                pattern: {^atom_name, ^unused_field2},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text2
              }
            ] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "ONLY emits warnings on files where a record has unused field(s)" do
-    bad_file = "bad.exs"
-    module = read_module_name("bad.exs")
-    files_and_asts = parse_files(["none.exs", "good.exs", bad_file])
+    bad_file = @test_directory_path <> "bad.exs"
+    module = TestHelpers.read_module_name(bad_file)
+
+    files_and_asts =
+      TestHelpers.parse_files([
+        @test_directory_path <> "none.exs",
+        @test_directory_path <> "good.exs",
+        bad_file
+      ])
+
     record = read_records(bad_file)
     [{camel_name, atom_name, fields}] = record
     [unused_field1, unused_field2] = fields
@@ -65,29 +72,29 @@ defmodule MeandroTest.Rule.UnusedRecordFields do
       "Record :#{atom_name} (#{camel_name}) has an unused field in the module: #{unused_field2}"
 
     assert [
-             %Meandro.Rule{
-               file: @test_directory_path <> ^bad_file,
+             %Rule{
+               file: ^bad_file,
                line: 3,
                module: ^module,
                pattern: {^atom_name, ^unused_field1},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text1
              },
-             %Meandro.Rule{
-               file: @test_directory_path <> ^bad_file,
+             %Rule{
+               file: ^bad_file,
                line: 3,
                module: ^module,
                pattern: {^atom_name, ^unused_field2},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text2
              }
            ] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "emits warnings on files with multiple records, only when there are unused fields" do
-    file = "mixed.exs"
-    module = read_module_name(file)
-    files_and_asts = parse_files([file])
+    file = @test_directory_path <> "mixed.exs"
+    module = TestHelpers.read_module_name(file)
+    files_and_asts = TestHelpers.parse_files([file])
     [record1, _record2] = read_records(file)
     {camel_name, atom_name, fields} = record1
     [unused_field] = Enum.filter(fields, fn s -> s == :unused end)
@@ -96,21 +103,21 @@ defmodule MeandroTest.Rule.UnusedRecordFields do
       "Record :#{atom_name} (#{camel_name}) has an unused field in the module: #{unused_field}"
 
     assert [
-             %Meandro.Rule{
-               file: @test_directory_path <> ^file,
+             %Rule{
+               file: ^file,
                line: 3,
                module: ^module,
                pattern: {^atom_name, ^unused_field},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text
              }
            ] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "emits warnings on files with multiple records, when there are only unused fields" do
-    file = "bad_multiple_records.exs"
-    module = read_module_name(file)
-    files_and_asts = parse_files([file])
+    file = @test_directory_path <> "bad_multiple_records.exs"
+    module = TestHelpers.read_module_name(file)
+    files_and_asts = TestHelpers.parse_files([file])
     [record1, record2] = read_records(file)
     {camel_name1, atom_name1, [unused_field1]} = record1
     {camel_name2, atom_name2, [unused_field2]} = record2
@@ -122,49 +129,34 @@ defmodule MeandroTest.Rule.UnusedRecordFields do
       "Record :#{atom_name2} (#{camel_name2}) has an unused field in the module: #{unused_field2}"
 
     assert [
-             %Meandro.Rule{
-               file: @test_directory_path <> ^file,
+             %Rule{
+               file: ^file,
                line: 3,
                module: ^module,
                pattern: {^atom_name1, ^unused_field1},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text1
              },
-             %Meandro.Rule{
-               file: @test_directory_path <> ^file,
+             %Rule{
+               file: ^file,
                line: 4,
                module: ^module,
                pattern: {^atom_name2, ^unused_field2},
-               rule: Meandro.Rule.UnusedRecordFields,
+               rule: UnusedRecordFields,
                text: ^expected_text2
              }
            ] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
   test "using record fields with each of the record functions/syntaxes counts as them being used" do
-    file = "exhaustive.exs"
-    files_and_asts = parse_files([file])
+    file = @test_directory_path <> "exhaustive.exs"
+    files_and_asts = TestHelpers.parse_files([file])
 
     assert [] = Rule.analyze(UnusedRecordFields, files_and_asts, :nocontext)
   end
 
-  defp parse_files(paths) do
-    files = for p <- paths, do: @test_directory_path <> p
-    Meandro.Util.parse_files(files, :sequential)
-  end
-
-  defp read_module_name(file_path) do
-    {:ok, contents} = File.read(@test_directory_path <> file_path)
-    pattern = ~r{defmodule \s+ ([^\s]+) }x
-
-    Regex.scan(pattern, contents, capture: :all_but_first)
-    |> List.flatten()
-    |> List.first()
-    |> String.to_atom()
-  end
-
   defp read_records(file_path) do
-    {:ok, contents} = File.read(@test_directory_path <> file_path)
+    {:ok, contents} = File.read(file_path)
     pattern = ~r{Record\.defrecordp?\([:a-zA-Z](.*)\)}x
 
     Regex.scan(pattern, contents, capture: :all_but_first)
